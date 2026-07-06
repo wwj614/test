9ecddb43b43f1173557b366ae733115ff98b98c9
wwj614 <wwj614@hotmail.com>
2026-07-06 10:59:34 +0800

    Update PermutationGenerator.cpp

--- PermutationGenerator.cpp
+++ PermutationGenerator.cpp
@@ -1,64 +1,116 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 
 class PermutationGenerator {
 private:
-  std::vector<int> path;
-  std::vector<bool> used;
-  std::vector<std::vector<int>> result;
-
 public:
-  void backtrack(const std::vector<int>& nums) {
-    if (path.size() == nums.size()) {
-      result.push_back(path);
+  int N;  //Matrix Rows, Cols
+  int *M;
+  int n;
+  int *route;
+  int minRoute;
+  int *minRoutes;
+
+  void p(int *route, int step, int s) {
+    for (int i=0; i <= step; ++i) {
+        std::cout << route[i] + 1 << " " ;
+    }
+    std::cout  << " ( " << s << " ) " << std::endl;
+  }
+  
+  void dup(int s) {
+    minRoute = s;
+    memcpy(minRoutes, route, n*sizeof(int));
+  }
+
+  void swap(int *first, int *second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+  }
+
+  void swap(int first, int second) {
+    int temp = route[first];
+    route[first] = route[second];
+    route[second] = temp;
+  }
+  
+  int sumRoute(int step, int s) {
+    if (step == 0) return s; 
+    return s + M[route[step-1]*N + route[step]];
+  }
+
+  void backtrack(int step, int s) {
+    if (step == n) {
+      s = sumRoute(step, s);
+      if (s >= minRoute) return;
+      dup(s);
+//      p(route,step,s);
       return;
     }
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (used[i]) continue;  // 已使用，跳过
-
-            // 选择
-      path.push_back(nums[i]);
-      used[i] = true;
-
-    for (int x : path) {
-      std::cout << x << " ";
+    for (int i = step; i <= n; ++i) {
+//      swap(&route[step], &route[i]);
+      swap(step, i);
+      backtrack(step+1, sumRoute(step, s));
+//      swap(&route[i], &route[step]);
+      swap(i, step);
     }
-    std::cout << "\n";
-            // 递归
-      backtrack(nums);
-      
-
-
-
-            // 撤销选择（回溯）
-      path.pop_back();
-      used[i] = false;
-      }
-    }
-
-  std::vector<std::vector<int>> generate(const std::vector<int>& nums) {
-    path.clear();
-    used.assign(nums.size(), false);
-    result.clear();
-    backtrack(nums);
-    return result;
   }
+
+  void generate() {
+    minRoute = 1<<20;
+    backtrack(0, 0);
+    p(route,n,minRoute);
+  }
+
 };
 
 //g++ --std=c++11 -m64 -O3 -o PermutationGenerator PermutationGenerator.cpp
-int main() {
-  std::vector<int> nums = {59,58,81,75,33};
-  PermutationGenerator pg;
-  auto perms = pg.generate(nums);
+int main(int argc, char* argv[]) {
+  char *inMareixStr = argv[1];
+  char *inRouteStr = argv[2];
+ 
+  PermutationGenerator pg;  
 
-  for (const auto& p : perms) {
-    for (int x : p) {
-      std::cout << x << " ";
-    }
-    std::cout << "\n";
+  std::ifstream matrixFile(inMareixStr);
+  if (!matrixFile.is_open()) {
+    std::cerr << "can not open matrix file: " << matrixFile << std::endl;
+    return 1;
   }
+  matrixFile >> pg.N;
+  pg.M = (int *)malloc(pg.N * pg.N * sizeof(int));
+  for (int row = 0; row < pg.N; ++row) {
+    for (int col = 0; col < pg.N; ++col) {
+      if (!(matrixFile >> pg.M[row * pg.N + col])) {
+        std::cerr << "read errow: data are not enough" << std::endl;
+        return 1;
+      }
+    }
+  }
+  matrixFile.close();
 
+  std::ifstream routeFile(inRouteStr);
+  if (!routeFile.is_open()) {
+    std::cerr << "can not open route file: " << inRouteStr << std::endl;
+    return 1;
+  }
+//  routeFile >> pg.n;
+//  int n = std::atoi(argv[3]); //Route Size
+  pg.n = std::atoi(argv[3]); //Route Size
+  pg.route = (int *)malloc(pg.n * sizeof(int));
+  pg.minRoutes = (int *)malloc(pg.n * sizeof(int));
+  pg.n--;
+  for (int i = 0; i <= pg.n; ++i) {
+    if (!(routeFile >> pg.route[i])) {
+      std::cerr << "read error: data are not enough" << std::endl;
+      return 1;
+    }
+    pg.route[i]--;
+  }
+  routeFile.close();
+  
+  pg.generate();
   return 0;
 }
